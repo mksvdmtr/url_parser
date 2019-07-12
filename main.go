004cb1ece5a97865aa6017b9d9fd3a5574a26f52
Dmitryi Maksakov <mksvdmtr@yandex.ru>
2019-07-12 10:15:38 +0300

    update

--- main.go
+++ main.go
@@ -9,34 +9,39 @@ import (
 )
 
 func main() {
-	urls := os.Args[1]
-	file, err := os.Open(urls) // open file with urls
-	if err != nil {
-		fmt.Println("Error", err)
-	}
 
-	result, err := os.Create("result.txt")
-	if err != nil {
-		fmt.Println(err)
-	}
+	if len(os.Args) == 1 {
+		fmt.Println("Please specify file")
+	} else {
 
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		resp, err := http.Head(scanner.Text())
-		time.Sleep(500 * time.Millisecond)
+		urls := os.Args[1]
+
+		file, err := os.Open(urls) // open file with urls
 		if err != nil {
-			fmt.Println("error", err)
-		} else {
-			id := resp.Request.URL.Path
-			hru := resp.Request.Header.Get("Referer")
-			result.WriteString(id + " -> " + hru + "\n")
-
-			fmt.Println(id, hru)
-
+			fmt.Println("Error", err)
 		}
 
-	}
-	file.Close()
-	result.Close()
+		result, err := os.Create("result.txt")
+		if err != nil {
+			fmt.Println(err)
+		}
 
+		scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			resp, err := http.Head(scanner.Text())
+			time.Sleep(500 * time.Millisecond)
+			if err != nil {
+				fmt.Println("error", err)
+			} else {
+				id := resp.Request.URL.String()
+				hru := resp.Request.Header.Get("Referer")
+				result.WriteString(hru + " -> " + id + "\n")
+				fmt.Printf("%s -> %s\n", hru, id)
+
+			}
+
+		}
+		file.Close()
+		result.Close()
+	}
 }
